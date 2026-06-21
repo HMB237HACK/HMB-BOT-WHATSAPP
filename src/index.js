a44357acd42cd727229cf0bd788014c76576a088
HMB237HACK <hamzalopere09@gmail.com>
2026-06-21 13:43:49 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+‎
+‎const WhatsAppClient = require('./core/whatsapp-client');
+‎const WebServer = require('./web/server');
+‎const Logger = require('./utils/logger');
+‎
+‎const logger = new Logger('Main');
+‎
+‎async function main() {
+‎    try {
+‎        logger.info('🚀 Démarrage...');
+‎        
+‎        const server = new WebServer();
+‎        await server.start();
+‎        
+‎        const whatsapp = new WhatsAppClient(server.io);
+‎        await whatsapp.initialize();
+‎        
+‎        process.on('SIGTERM', async () => {
+‎            logger.info('Arrêt...');
+‎            await whatsapp.disconnect();
+‎            await server.stop();
+‎            process.exit(0);
+‎        });
+‎        
+‎    } catch (error) {
+‎        logger.error(`Erreur: ${error.message}`);
+‎        process.exit(1);
+‎    }
+‎}
+‎
+‎main();
